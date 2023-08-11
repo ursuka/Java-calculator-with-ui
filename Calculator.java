3fd0b57ea6599a14e0e39a9bb4588f003cb9e587
Fusa Vladislav <fusavlad@gmail.com>
2023-08-11 17:59:23 +0300

    Update 0.2: -adding a text field to the calculator

--- Calculator.java
+++ Calculator.java
@@ -26,7 +26,11 @@ public class Calculator implements ActionListener {
         frame.setSize(420, 550);
         frame.setLayout(null);
 
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
 
+        frame.add(textField);
         frame.setVisible(true);
     }
 
